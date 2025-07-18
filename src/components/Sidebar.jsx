8f3cff6fea3660d44ea1bf44d8d8327a81f72d57
Ahror <axrorwillberich2230@gmail.com>
2025-07-18 18:51:26 +0500

    First commit

---
+++ src/components/Sidebar.jsx
@@ -0,0 +1,156 @@
+import React, {useEffect, useState} from "react";
+import {NavLink, useLocation} from "react-router-dom";
+import {RiFolderAddLine} from "react-icons/ri";
+import {CiViewList} from "react-icons/ci";
+import { PiListChecksFill } from "react-icons/pi";
+import logo from "../assets/images/logo_sharq.png";
+import {IoIosPerson} from "react-icons/io";
+import {
+    Home,
+    ChevronRight,
+    ChevronDown,
+} from "lucide-react";
+import { MdLanguage } from "react-icons/md";
+import {FaList} from "react-icons/fa6";
+import {LuNotebookText} from "react-icons/lu";
+import {MdOutlinePostAdd} from "react-icons/md";
+import {CgProfile} from "react-icons/cg";
+import {jwtDecode} from "jwt-decode";
+
+function Sidebar({isOpen}) {
+    const location = useLocation();
+    const [expandedCategories, setExpandedCategories] = useState(["/"]);
+    const token = JSON.parse(localStorage.getItem("token"));
+    const [userRole, setUserRole] = useState("");
+    useEffect(() => {
+        const token = localStorage.getItem("token");
+        if (token) {
+            const decoded = jwtDecode(token);
+            setUserRole(decoded?.role);
+        } else {
+            setUserRole(null);
+        }
+    }, [localStorage.getItem("token")]);
+    const toggleCategory = (category) => {
+        setExpandedCategories((prev) =>
+            prev.includes(category)
+                ? prev.filter((c) => c !== category)
+                : [...prev, category]
+        );
+    };
+    let menuCategories;
+    // if (userRole === "admin") {
+        menuCategories = [
+            // {
+            //     id: "main",
+            //     items: [{icon: Home, label: "Bosh sahifa", path: "/admin"}],
+            // },
+            {
+                id: "application",
+                items: [
+                    {icon: PiListChecksFill , label: "Arizalar", path: "/list-application"},
+                ],
+            },
+            {
+                id: "profile",
+                items: [
+                    {icon: IoIosPerson , label: "Profil", path: "/profile"},
+                ],
+            },
+    //         {
+    //             id: "no_list",
+    //             items: [
+    //                 {icon: FaList, label: "Rad bo'lgan ", path: "/no-list-application"},
+    //             ],
+    //         },
+    //     ];
+    // } else if (userRole === "student") {
+    //     menuCategories = [
+    //         {
+    //             id: "application",
+    //             items: [{icon: MdOutlinePostAdd, label: "Arizalar", path: "/application"}],
+    //         },
+    //         {
+    //             id: "rating",
+    //             items: [{icon: LuNotebookText, label: "Reyting daftarcha", path: "/rating"}],
+    //         },
+    //         {
+    //             id: "profile",
+    //             items: [{icon: CgProfile, label: "Profil", path: "/profile"}],
+    //         },
+        ];
+    // }
+
+
+    return (
+        <aside
+            className={`fixed bottom-0 w-full h-[calc(10vh)] sm:left-0 sm:top-0 sm:h-[calc(100vh)] bg-white shadow-lg transition-all duration-300 ${
+                isOpen ? "sm:w-64" : "sm:w-20"
+            } z-10 overflow-y-auto`}
+        >
+            <nav className="p-4 flex sm:flex-col">
+                <div className="hidden sm:flex sm:items-center sm:justify-center pb-4  px-16 border-b-2">
+                    <img src={logo} alt=""/>
+                </div>
+                {menuCategories?.map((category) => (
+                    <div key={category.id} className="my-1">
+                        {category.title && isOpen && (
+                            <button
+                                onClick={() => toggleCategory(category.id)}
+                                className="flex items-center justify-between w-full px-3 py-2 text-sm font-medium text-gray-500"
+                            >
+                                {category.title}
+                                {expandedCategories.includes(category.id) ? (
+                                    <ChevronDown className="h-4 w-4"/>
+                                ) : (
+                                    <ChevronRight className="h-4 w-4"/>
+                                )}
+                            </button>
+                        )}
+                        <ul
+                            className={`space-y-1 ${
+                                category.title &&
+                                !expandedCategories.includes(category.id) &&
+                                isOpen
+                                    ? "hidden"
+                                    : ""
+                            }`}
+                        >
+                            {category.items.map((item, index) => (
+                                <li key={index}>
+                                    <NavLink
+                                        to={item.path}
+                                        className={({isActive}) => `
+                      flex items-center p-3 rounded-lg  transition-all duration-300
+                      ${
+                                            isActive
+                                                ? "bg-[#E7E7FF] text-[#696CFF] font-semibold"
+                                                : "text-gray-700 hover:bg-gray-100"
+                                        }
+                    `}
+                                    >
+                                        <item.icon
+                                            className={`h-5 w-5 ${
+                                                location.pathname === item.path
+                                                    ? "text-[#2557A7]"
+                                                    : "text-gray-500"
+                                            }`}
+                                        />
+                                        <span className={`ml-3 ${!isOpen ? "hidden" : ""}`}>
+                      {item.label}
+                    </span>
+                                        {location.pathname === item.path && isOpen && (
+                                            <ChevronRight className="ml-auto h-4 w-4 text-[#2557A7]"/>
+                                        )}
+                                    </NavLink>
+                                </li>
+                            ))}
+                        </ul>
+                    </div>
+                ))}
+            </nav>
+        </aside>
+    );
+}
+
+export default Sidebar;
---
+++ src/pages/LoginPage.jsx
@@ -0,0 +1,159 @@
+import {useEffect, useState} from "react";
+import {
+    Card,
+    Input,
+    Checkbox,
+    // Button,
+    Typography,
+} from "@material-tailwind/react";
+import {IMaskInput} from 'react-imask';
+import {useNavigate, Link} from "react-router-dom";
+import toast from "react-hot-toast";
+import {useMutation, useQuery, useQueryClient} from "@tanstack/react-query";
+
+import {replace, useFormik} from "formik";
+import * as Yup from "yup";
+import {LoginApi} from "../Api/LoginApi";
+import {LogIn} from 'lucide-react'
+import {GrHide} from "react-icons/gr";
+import {BiShow} from "react-icons/bi";
+import {FaArrowRightLong} from "react-icons/fa6";
+
+import Logo from "../assets/images/logo.png";
+import Sharq from "../assets/images/sharq.jpg";
+import {GetPassportApi} from "../Api/UserApi.jsx";
+
+export default function LoginPage() {
+    const navigate = useNavigate();
+    const queryClient = useQueryClient();
+
+    const [showPassword, setShowPassword] = useState(false);
+
+
+    const togglePassword = () => {
+        setShowPassword((prev) => !prev);
+    };
+    const loginMutation = useMutation({
+        mutationKey: ["login-user"],
+        mutationFn: LoginApi,
+        onSuccess: (data) => {
+            toast.success(data.message || "Successfully");
+            queryClient.clear();
+        },
+        onError: (error) => {
+            toast.error(error.message);
+        },
+    });
+
+    const isSuccess = loginMutation.isSuccess;
+
+    useEffect(() => {
+        if (isSuccess) {
+            navigate("/profile");
+        }
+    }, [navigate, isSuccess]);
+    const formik = useFormik({
+        initialValues: {
+            username: "",
+            password: "",
+        },
+        validationSchema: Yup.object({
+            username: Yup.string().required("Telefon raqami majburiy"),
+            password: Yup.string().required("Parol majburiy"),
+        }),
+        onSubmit: (values) => {
+            const loginData = {
+                username: values.username,
+                password: values.password,
+            };
+            loginMutation.mutate(loginData);
+        },
+    });
+    return (
+        <div className="bg-gray-100 w-full flex mt-[10%] min-h-[80%] justify-center items-center  overflow-hidden">
+            <div
+                className="bg-white rounded-lg shadow-lg w-full max-w-6xl h-[80%] md:h-[80%] flex flex-col md:flex-row overflow-hidden">
+                {/* Chap qism */}
+                <div className="md:w-1/2 p-8 flex flex-col justify-center">
+                    <div className="flex justify-center mb-6">
+                        <img src={Logo} className="w-44"/>
+                    </div>
+                    <h2 className="text-2xl font-bold text-center mb-6">
+                        Kabenitga kirish
+                    </h2>
+
+
+                    <form onSubmit={formik.handleSubmit}>
+                        <div className="mb-4">
+                            <label className="block text-gray-700 text-sm font-semibold mb-2">
+                                Telefon raqam
+                            </label>
+                            <IMaskInput
+                                mask="+{998}-00-000-00-00"
+                                name="username"
+                                value={formik.values.username}
+                                onChange={(e) => { // inputdan faqat raqamlarni ajratib olish
+                                    const onlyDigits = e.target.value.replace(/\D/g, '');
+                                    formik.setFieldValue("username", onlyDigits);
+                                }}
+                                onBlur={formik.handleBlur}
+                                className="border rounded px-3 py-2 w-full"
+                                placeholder="+998 (__) ___-__-__"
+                                required
+                            />
+                        </div>
+                        <div className="mb-6">
+                            <label className="block text-gray-700 text-sm font-semibold mb-2">
+                                Parol
+                            </label>
+                            <div className="relative">
+                                <input
+                                    type={showPassword ? "text" : "password"}
+                                    name="password"
+                                    {...formik.getFieldProps("password")}
+                                    className=" w-full  px-3 py-2 border border-gray-300 rounded focus:outline-none focus:border-blue-500"
+                                    required
+                                />
+                                <button
+                                    type="button"
+                                    onClick={togglePassword}
+                                    className="absolute right-2 top-3 text-xl items-center  text-blue-500"
+                                >
+                                    {showPassword ? <GrHide/> : <BiShow/>}
+                                </button>
+                            </div>
+                        </div>
+                        <button
+                            type="submit"
+                            className="w-full bg-blue-500 text-white py-2 px-4 rounded hover:bg-blue-600 transition duration-200 font-semibold"
+                        >
+                            Kirish
+                        </button>
+                    </form>
+                    {/*<div className="text-center mt-4">*/}
+                    {/*    <Link to="/verify-sms"*/}
+                    {/*          className="flex justify-center items-center font-semibold text-gray-500 hover:underline pl-3">*/}
+                    {/*        Ro'yxatdan o'tish <FaArrowRightLong className="ml-2 text-blue-500"/>*/}
+                    {/*    </Link>*/}
+                    {/*</div>*/}
+                    {/*<div className="text-center mt-4">*/}
+                    {/*    <Link to="/forget-password"*/}
+                    {/*          className="flex justify-center items-center font-semibold text-gray-500 hover:underline pl-3">*/}
+                    {/*        Parolni unutdingizmi <FaArrowRightLong className="ml-2 text-blue-500"/>*/}
+                    {/*    </Link>*/}
+                    {/*</div>*/}
+                </div>
+
+                {/* O‘ng qism */}
+                <div className="relative  md:w-1/2 flex  justify-center items-center bg-[#1c4c84] rounded-lg">
+                    <img src={Sharq} className="w-full h-full  object-coverv  rounded-tl-lg rounded-bl-lg"
+                         alt=""/>
+                    <div
+                        className="absolute opacity-0 opacity-100 flex bottom-0 left-0 w-full bg-white/15 backdrop-blur-md px-4 py-6 justify-center border-t-2 rounded-bl-lg">
+                        <h2 className="text-white text-4xl md:text-4xl font-bold">Sharq universiteti</h2>
+                    </div>
+                </div>
+            </div>
+        </div>
+    )
+}
